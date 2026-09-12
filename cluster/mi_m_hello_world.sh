a3146c4e8f314556863c78fca832282250216739
mike_IB <143018639+miguel-isidro05@users.noreply.github.com>
2026-09-11 22:31:38 -0400

    Update mi_m_hello_world.sh

--- cluster/mi_m_hello_world.sh
+++ cluster/mi_m_hello_world.sh
@@ -3,7 +3,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
-#SBATCH --nodelist=worker8
+#SBATCH --nodelist=worker7
 #SBATCH --output="mi_m_hello_world.out"
 
 srun matlab -nosplash -nodesktop -nodisplay -r "mi_m_hello_world; exit"
