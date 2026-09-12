dfea4f66b9bc42d87553af121e13c46a05d47f53
mike_IB <143018639+miguel-isidro05@users.noreply.github.com>
2026-09-11 22:32:07 -0400

    Change nodelist from worker8 to worker7

--- cluster/mi_m_kwavebaseline.sh
+++ cluster/mi_m_kwavebaseline.sh
@@ -3,7 +3,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --nodes=1
 #SBATCH --partition=thinkstation
-#SBATCH --nodelist=worker8
+#SBATCH --nodelist=worker7
 #SBATCH --output="mi_m_kwave_baseline-%j.out"
 
 srun matlab -nosplash -nodesktop -nodisplay -r "mi_m_kwavebaseline; exit"
